935cdc379a76a05419b891ce5811c25f71318ffb
Timothy Condon <151268@cityhall.boston.cob>
2018-12-21 17:13:34 -0500

    Add in units to capture more than just primary SAM Id

--- prep_table_bpda.R
+++ prep_table_bpda.R
@@ -20,12 +20,23 @@ available <- gs_ls()
 str.connect <- gs_title("BPDA restricted condos 2018_12_21")
 bpda <- gs_read(str.connect)
 
+bpda$unit_id <- as.character(bpda$`Unit #`)
+bpda$unit_id <- paste0("#", bpda$unit_id, sep = "")
+
 bpda.address <- unite(bpda, Address, 
                      c("Street #", "Street Name", "Street suffix", "ZIP"), 
                      sep = " ", 
                      remove = FALSE)
 
+bpda.address.unit <- unite(bpda, Address, 
+                      c("Street #", "Street Name", "Street suffix", "unit_id", "ZIP"), 
+                      sep = " ", 
+                      remove = FALSE)
+
 write.csv(bpda.address, "bpda_addresses.csv", row.names = FALSE)
+write.csv(bpda.address.unit, "bpda_addresses_unit.csv", row.names = FALSE)
+
+## Run 1
 
 bpda.run1 <- read.xlsx("bpda_run1.xlsx", 1)
 bpda.sam.good <- subset(bpda.run1, Score > 60)
@@ -100,6 +111,43 @@ write.xlsx(bpda.address.geo.all, "BPDA_restricted_condos_2018_12_21_SAMID.xlsx",
 
 
 
+####### Units included
+bpda.run1.units <- read.csv("bpda_run1_units_included.csv")
+bpda.run1.units <- bpda.run1.units[1:1053, 1:18]
+bpda.run1.units.good <- subset(bpda.run1.units, bpda.run1.units$Match.Score > 4)
+bpda.run1.units.bad <- subset(bpda.run1.units, bpda.run1.units$Match.Score == 0)
+
+# good
+
+bpda.address.units.good <- data.table(bpda.run1.units.good$Street..,
+                                    bpda.run1.units.good$Street.Name,
+                                    bpda.run1.units.good$Street.suffix,
+                                    bpda.run1.units.good$Unit..,
+                                    bpda.run1.units.good$ZIP,
+                                    bpda.run1.units.good$Match.Id,
+                                    bpda.run1.units.good$Address)
+colnames(bpda.address.units.good) <- c('Street #', 'Street Name',
+                                     'Street suffix', 'Unit #', 'ZIP', 'SAM ID',
+                                     'Address')
+
+# bad
+
+bpda.address.units.bad <- data.table(bpda.run1.units.bad$Street..,
+                                      bpda.run1.units.bad$Street.Name,
+                                      bpda.run1.units.bad$Street.suffix,
+                                      bpda.run1.units.bad$Unit..,
+                                      bpda.run1.units.bad$ZIP,
+                                      bpda.run1.units.bad$Match.Id,
+                                      bpda.run1.units.bad$Address)
+colnames(bpda.address.units.bad) <- c('Street #', 'Street Name',
+                                       'Street suffix', 'Unit #', 'ZIP', 'SAM ID',
+                                       'Address')
+
+# make sam id na for bad
+bpda.address.units.bad$`SAM ID` <- NA
+
+# rbind them together
+bpda.units <- rbind(bpda.address.units.good, bpda.address.units.bad)
 
 
 
